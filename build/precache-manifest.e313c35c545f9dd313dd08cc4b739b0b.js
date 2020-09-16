31611bcd3733cc3cb29d6de7a0683795b90da48a
Juan Pablo Ramos Robles <65674901+JPRR44@users.noreply.github.com>
2020-09-15 23:46:25 -0500

    Add files via upload
    Feat: Add optimized version

---
+++ build/precache-manifest.e313c35c545f9dd313dd08cc4b739b0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5b2f30ccdb47ade6c9aa853a52de44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35f1394fbdf6e18e4424",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4700eb3e706b2f23b7ae",
+    "url": "/static/css/main.ddf04751.chunk.css"
+  },
+  {
+    "revision": "35f1394fbdf6e18e4424",
+    "url": "/static/js/2.c7dd68e1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c7dd68e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4700eb3e706b2f23b7ae",
+    "url": "/static/js/main.68d19f96.chunk.js"
+  },
+  {
+    "revision": "15f82c8e9fee091d108b",
+    "url": "/static/js/runtime-main.722cfe37.js"
+  }
+]);
